a2aecff3760dd1a99d7548598fde40f92acadc91
Mirna El Tohamy <m.tohamy.2000@gmail.com>
2026-02-20 23:21:24 +0200

    p1q1

--- OOPAssignment2/Program.cs
+++ OOPAssignment2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region p1q1
+//a
+//public fields allow external modifications & no proper validation in withdraw method (to check balance availability)
+
+//b
+//make fields private & provide setters and getters instead
+//validate whether balance is sufficient before allowing withdrawal
+
+//c
+//because public fields allow uncontrolled modifications which can lead to invalid state of object
+#endregion
